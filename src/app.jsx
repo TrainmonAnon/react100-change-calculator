3d6a9b18f12f11418872f7d2da06ee9019a3da63
TrainmonAnon <36116671+TrainmonAnon@users.noreply.github.com>
2019-05-15 15:40:47 -0700

    update app visuals

--- src/app.jsx
+++ src/app.jsx
@@ -64,71 +64,80 @@ class App extends Component {
   render() {
     return (
       <div className='container'>
-        <div className='page-header'>
-          <h1>Change Calulator</h1>
+        <div className='page-header text-white'>
+          <h1 className=''>Change Calulator</h1>
+          <hr />
         </div>
 
         <div className='row'>
-          <div className='col-4 well'>
-            <div className='panel panel-default bg-light'>
-              <div className='panel-heading'>Enter Information</div>
-              <div className='panel-body bg-white'>
-                <p>How much is due?</p>
-                <input
-                  name='amountDue' id='due' min='0' step='0.01' type='number'
-                  onChange={ e => this.handleChange(e) }
-                />
-                <p>How much was received?</p>
-                <input
-                  name='amountReceived' id='received' min='0' step='0.01' type='number' 
-                  onChange={ e => this.handleChange(e) } 
-                />
+          <div className='col-4'>
+            <div className='card'>
+              <div className='card-header'>Enter Information</div>
+              <div className='card-body bg-white'>
+                <form>
+                  <div className='form-group'>
+                    <p className='font-weight-bold'>How much is due?</p>
+                    <input
+                      name='amountDue' className='form-control'
+                      id='due' min='0' step='0.01' type='number'
+                      onChange={ e => this.handleChange(e) }
+                    />
+                    <p className='font-weight-bold'>How much was received?</p>
+                    <input
+                      name='amountReceived' className='form-control'
+                      id='received' min='0' step='0.01' type='number'
+                      onChange={ e => this.handleChange(e) }
+                    />
+                  </div>
+                </form>
               </div>
-              <div className='panel-footer'>
+              <div className='card-footer'>
                 <button
-                  type='button' className='btn' id='calculate' 
+                  type='button' className='btn-primary btn-block rounded' id='calculate'
                   onClick={ () => this.calculate() }
                 >Calculate</button>
               </div>
             </div>
           </div>
 
-          <div className='col-8 well bg-white'>
-            <div className={ 'alert alert-' + (this.state.moreDue ? 'warning' : 'success') } role='alert'>
-              { this.state.moreDue ? 'Additional money owed.' : ('The total change due is $' + this.state.change) }
+          <div className='col-8 bg-white'>
+            <br />
+            <div className={ `alert alert-${this.state.moreDue ? 'warning' : 'success'}` } role='alert'>
+              { this.state.moreDue ? 'Additional money owed.' : (`The total change due is $${this.state.change}`) }
             </div>
 
-            <div className='row'>
-              <div className='col well text-center bg-light'>
+            <div className='row text-center'>
+              <div className='col-3 rounded'>
                 <h1>Twenties</h1>
                 <p className='change'>{this.state.twenties}</p>
               </div>
-              <div className='col well text-center bg-light'>
+              <div className='col-3 rounded'>
                 <h1>Tens</h1>
                 <p className='change'>{this.state.tens}</p>
               </div>
-              <div className='col well text-center bg-light'>
+              <div className='col-3 rounded'>
                 <h1>Fives</h1>
-                <p className='col change'>{this.state.fives}</p>
+                <p className='change'>{this.state.fives}</p>
               </div>
-              <div className='col well text-center bg-light'>
+              <div className='col-3 rounded'>
                 <h1>Ones</h1>
                 <p className='change'>{this.state.ones}</p>
               </div>
-              <div className='w-100' />
-              <div className='col well text-center bg-light'>
+            </div>
+            <div className='row text-center'>
+              <div className='col-3 rounded'>
                 <h1>Quarters</h1>
                 <p className='change'>{this.state.quarters}</p>
               </div>
-              <div className='col well text-center bg-light'>
+              <div className='col-3 rounded'>
                 <h1>Dimes</h1>
                 <p className='change'>{this.state.dimes}</p>
               </div>
-              <div className='col well text-center bg-light'>
+              <div className='col-3 rounded'>
                 <h1>Nickels</h1>
                 <p className='change'>{this.state.nickels}</p>
               </div>
-              <div className='col well text-center bg-light'>
+              <div className='col-3 rounded'>
                 <h1>Pennies</h1>
                 <p className='change'>{this.state.pennies}</p>
               </div>
